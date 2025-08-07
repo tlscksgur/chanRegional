ecf4ee7408ff385be6f769addb21cdfe8f2583b4
tlscksgur <12880924a@gmail.com>
2025-08-07 17:01:51 +0900

    많은걸함

---
+++ A-Module/js/script.js
@@ -0,0 +1,18 @@
+//fetch("./js/product.json")
+//.then(res => res.json())
+//.then(data => {
+//  const product = document.querySelector(".product")
+//
+//  Object.values(data.product).flat().forEach(e => {
+//    product.innerHTML += `
+//      <ul>
+//        <li class="title">${e.title}</li>
+//        <li class="img"><img src="${e.img}" alt="${e.title}"></li>
+//        <li class="content">${e.content}</li>
+//        <li class="price">${e.price}</li>
+//      </ul>
+//      <button class="buyBtn">구매하기</button><button class="cartBtn">장바구니</button>
+//    `
+//  });
+//
+//})
